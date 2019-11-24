c3b0e8298127bcb46086de40f9fc96cdbd4a3e93
Albert Ramos <seyn@outlook.com>
2019-11-24 01:08:36 +0100

    analytics

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-153174385-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-153174385-1');
+        </script>
+
         <meta charset="utf-8">
         <title>Albert Ramos</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
